b57e2f49f2ad601c5eef13f324909112ff44c908
cloverbee <xyza8992@gmail.com>
2019-12-13 14:29:34 -0800

    Update database.js

--- express/config/database.js
+++ express/config/database.js
@@ -3,7 +3,7 @@ const db = mongoose.connection;
 
 function init() {
   //mongoose.connect("mongodb://127.0.0.1:27017/employee");
-  //mongoose.connect('mongodb://Elaine:××××××@cluster0-shard-00-00-qm0uv.gcp.mongodb.net:27017,cluster0-shard-00-01-qm0uv.gcp.mongodb.net:27017,cluster0-shard-00-02-qm0uv.gcp.mongodb.net:27017/1stProject?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true');
+  //mongoose.connect('mongodb://E）））））））））））:××××××@cluster0-shard-00-00-qm0uv.gcp.mongodb.net:27017,cluster0-shard-00-01-qm0uv.gcp.mongodb.net:27017,cluster0-shard-00-02-qm0uv.gcp.mongodb.net:27017/1stProject?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true');
 
 }
 
